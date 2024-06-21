df7ce68ef0e5ccecb7a6a2ddf23df7d5f2396c55
Thiago <thiago.geremias@compasso.com.br>
2024-06-21 12:34:17 -0300

    Add new tests and endpoints

--- locust/locustfile.py
+++ locust/locustfile.py
@@ -1,13 +1,39 @@
-import time
-from locust import HttpUser, task, constant
+from locust import HttpUser, TaskSet, task, between
+import random
 
-class QuickstartUser(HttpUser):
-    wait_time = constant(2)
-  
+class UserBehavior(TaskSet):
+
     @task(1)
-    def index(self):
+    def get_root(self):
         self.client.get("/")
-    
-    @task(2)
-    def random_pet(self):
-        self.client.get("/pets/random")
+
+    @task(3)
+    def checkout(self):
+        self.client.get("/pets/checkout")
+
+    @task(4)
+    def get_pet_product(self):
+        id_value = 10101 if random.randint(1, 10) <= 2 else random.randint(1, 10000)
+        self.client.get(f"/pets/products/{id_value}")
+
+    @task(5)
+    def check_stock(self):
+        product_id = random.randint(1, 10000)
+        self.client.get(f"/pets/check_stock/{product_id}")
+
+    @task(6)
+    def login(self):
+        # Payloads para injeção SQL
+        payloads = [
+            {"login": "admin' --", "password": "anything"},
+            {"login": "admin' OR 1=1 --", "password": "anything"},
+            {"login": "admin'; DROP TABLE users; --", "password": "anything"},
+            # Adicione mais payloads conforme necessário
+        ]
+
+        for payload in payloads:
+            self.client.post("/login", json=payload)
+
+class WebsiteUser(HttpUser):
+    tasks = [UserBehavior]
+    wait_time = between(1, 5)
--- pets/main.py
+++ pets/main.py
@@ -1,6 +1,8 @@
 from fastapi import FastAPI, HTTPException
 import httpx
 import random
+import asyncio
+from pydantic import BaseModel
 from opentelemetry import trace
 from opentelemetry.sdk.resources import Resource
 from opentelemetry.sdk.trace import TracerProvider
@@ -29,21 +31,84 @@ HTTPXClientInstrumentor().instrument()
 async def read_root():
     return {"message": "Au! Au!"}
 
-@app.get("/pets/random")
-async def get_random_pet():
+async def zipcode_check():
     async with httpx.AsyncClient() as client:
         try:
-            response = await client.get("https://pokeapi.co/api/v2/pokemon?limit=1")
+            await asyncio.sleep(40)
+            response = await client.get(f"https://viacep.com.br/ws/80410201/json/")
             response.raise_for_status()
-            total_pokemon = response.json()["count"]
-            random_id = random.randint(1, total_pokemon)
-            response = await client.get(f"https://pokeapi.co/api/v2/pokemon/{random_id}")
-            response.raise_for_status()
-            pokemon = response.json()
-            return {"name": pokemon["name"]}
+            data = response.json()
+            if "erro" in data:
+                raise HTTPException(status_code=404, detail="Zip code not found")
+            return "Checkout successful"
         except httpx.HTTPStatusError as e:
-            raise HTTPException(status_code=e.response.status_code, detail="Error fetching random pet")
+            raise HTTPException(status_code=e.response.status_code, detail="Error fetching zip code")
         except Exception as e:
             raise HTTPException(status_code=500, detail="Internal server error")
+
+@app.get("/pets/checkout")
+async def checkout():
+    result = await zipcode_check()
+    return {"message": result}
+
+# Lista de produtos de pet shop
+pet_products = [
+    "Dog Toy",
+    "Cat Food",
+    "Bird Cage",
+    "Fish Tank",
+    "Hamster Wheel"
+]
+
+@app.get("/pets/products/{id}")
+async def get_pet_product(id: int):
+    if id == 10101:
+        raise HTTPException(status_code=500, detail="Internal server error")
+    product = random.choice(pet_products)
+    return {"product": product}
+
+# Métodos de verificação de estoque
+async def physical_store(product_id: int):
+    # Simula a verificação de estoque na loja física
+    await asyncio.sleep(random.uniform(0.1, 0.5))  # Simula latência
+    return {"physical_store": random.choice([True, False])}
+
+async def third_party_stock(product_id: int):
+    # Simula a verificação de estoque em terceiros
+    await asyncio.sleep(random.uniform(0.1, 0.5))  # Simula latência
+    return {"third_party_stock": random.choice([True, False])}
+
+async def central_warehouse(product_id: int):
+    # Simula a verificação de estoque no armazém central
+    await asyncio.sleep(random.uniform(0.1, 0.5))  # Simula latência
+    raise HTTPException(status_code=500, detail="Internal server error")
+
+@app.get("/pets/check_stock/{product_id}")
+async def check_stock(product_id: int):
+    try:
+        # Executa as três verificações de estoque em paralelo
+        results = await asyncio.gather(
+            physical_store(product_id),
+            third_party_stock(product_id),
+            central_warehouse(product_id)
+        )
         
+        # Agrega os resultados em um único dicionário
+        stock_status = {k: v for d in results for k, v in d.items()}
+        
+        return stock_status
+    except Exception as e:
+        raise HTTPException(status_code=500, detail="Internal server error")
+    
+class LoginData(BaseModel):
+    login: str
+    password: str
+
+@app.post("/login")
+async def login(data: LoginData):
+    # Aqui você pode adicionar lógica de autenticação, como verificar o login e senha em um banco de dados.
+    if data.login == "admin" and data.password == "secret":
+        return {"message": "Login successful"}
+    else:
+        raise HTTPException(status_code=401, detail="Invalid login or password")
 
